aed32f0299741ded2d963883507ed9d2c0e6a3d9
Viktor Holta <viktor.holta@outlook.com>
2022-01-31 15:43:40 +0100

    Add prints with color

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, \
     SOMAXCONN, timeout
 from threading import Thread
 
+import color
 from message import Message, timeout_message
 
 
@@ -14,8 +15,6 @@ def send_request(request: Message) -> Message:
     if b':' in host:
         host, port = host.split(b':')
         port = int(port)
-        print(request)
-    #    return None
 
     s.connect((host, port))
     s.sendall(bytes(request))
@@ -31,7 +30,7 @@ def send_request(request: Message) -> Message:
     return Message(response_bytes)
     
 
-def request_handler(connection: socket):
+def request_handler(id: int, connection: socket):
     request = Message(connection.recv(4096))
     
     if request.is_valid:
@@ -39,27 +38,39 @@ def request_handler(connection: socket):
             Gecko/20100101 Firefox/96.0')
 
         try:
+            print(f'{id}: Sending {color.BOLD}({request.line.decode()}){color.END}...')
             response = send_request(request)
         except timeout:
             response = timeout_message(request)
             
         if response is not None:
+            _, status_code, _ = response.line.split(b' ', 2)
+            status_color = color.GREEN
+            if status_code.startswith(b'3'):
+                status_color = color.YELLOW
+            elif status_code.startswith(b'4') or status_code.startswith(b'5'):
+                status_color = color.RED
+
+            print(f'{id}: {status_color}Received {color.BOLD}({response.line.decode()}){color.END}!')
             connection.sendall(bytes(response))
 
     connection.close()
 
 
 def start_server(host: str, port: int):
+    print('Starting server...')
     s = socket(AF_INET, SOCK_STREAM)
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind((host, port))
-
     s.listen(SOMAXCONN)
-
+    print('Server started!\r\n')
+    
+    thread_id = 0
     while True:
         connection, connection_addr = s.accept()
-        thread = Thread(target=request_handler, args=[connection], daemon=True)
+        thread = Thread(target=request_handler, args=[thread_id, connection], daemon=True)
         thread.start()
+        thread_id += 1
 
 
 if __name__ == '__main__':
