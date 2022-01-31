55c85fbd2377e148a86c1ddab3b2824d23cf6b67
Viktor Holta <viktor.holta@outlook.com>
2022-01-31 14:45:35 +0100

    Finish sending requests and receiving responses

--- src/main.py
+++ src/main.py
@@ -1,21 +1,55 @@
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, \
-    SOMAXCONN
+    SOMAXCONN, timeout
 from threading import Thread
 
-from message import Request
+from message import Message, timeout_message
 
+
+def send_request(request: Message) -> Message:
+    s = socket(AF_INET, SOCK_STREAM)
+    s.settimeout(12)
+
+    host = request.get_header(b'Host')
+    port = 80
+    if b':' in host:
+        host, port = host.split(b':')
+        port = int(port)
+        print(request)
+    #    return None
+
+    s.connect((host, port))
+    s.sendall(bytes(request))
+
+    response_bytes = b''
+    while True:
+        data = s.recv(1024)
+        if len(data) <= 0:
+            break
+        response_bytes += data
+
+    s.close()
+    return Message(response_bytes)
+    
 
 def request_handler(connection: socket):
-    request = Request(connection.recv(4096))
+    request = Message(connection.recv(4096))
     
     if request.is_valid:
-        print(request)
-        request.set_header(b'User-Agent', b'Mozilla/5.0 (Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0')
+        request.set_header(b'User-Agent', b'Mozilla/5.0 (Linux x86_64; rv:96.0) \
+            Gecko/20100101 Firefox/96.0')
+
+        try:
+            response = send_request(request)
+        except timeout:
+            response = timeout_message(request)
+            
+        if response is not None:
+            connection.sendall(bytes(response))
 
     connection.close()
 
 
-def main(host: str, port: int):
+def start_server(host: str, port: int):
     s = socket(AF_INET, SOCK_STREAM)
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind((host, port))
@@ -29,4 +63,4 @@ def main(host: str, port: int):
 
 
 if __name__ == '__main__':
-    main('127.0.0.1', 8080)
+    start_server('127.0.0.1', 8080)
--- src/message.py
+++ src/message.py
@@ -1,16 +1,14 @@
-class Request:
+class Message:
     def __init__(self, byte_str: bytes):
         self.is_valid = byte_str != b''
 
         if byte_str != b'':
-            request_line, byte_str = byte_str.split(b'\r\n', 1)
+            self.line, byte_str = byte_str.split(b'\r\n', 1)
             header_fields, self.body = byte_str.split(b'\r\n\r\n', 1)
             
-            self.method, self.url, self.version = request_line.split(b' ')
             self.headers = dict()
-
             for header in header_fields.split(b'\r\n'):
-                header_key, header_val = header.split(b': ')
+                header_key, header_val = header.split(b': ', 1)
                 self.headers[header_key] = header_val
 
     def set_header(self, key: bytes, value: bytes):
@@ -23,14 +21,28 @@ class Request:
         return self.headers.get(key, None)
 
     def __bytes__(self) -> bytes:
-        value = self.method + b' ' + self.url + b' ' + self.version + b'\r\n'
+        value = self.line + b'\r\n'
         for header_key, header_val in self.headers.items():
             value += header_key + b': ' + header_val + b'\r\n'
         value += b'\r\n' + self.body
         return value
 
     def __str__(self) -> str:
+        bodyless = self.line + b'\r\n'
+        for header_key, header_val in self.headers.items():
+            bodyless += header_key + b': ' + header_val + b'\r\n'
+
         value = '-' * 64 + '\r\n'
-        value += self.__bytes__().decode('utf-8')
+        value += bodyless.decode()
         value += '-' * 64
         return value
+
+
+def timeout_message(request: Message):
+    _, _, version = request.line.split(b' ', 2)
+    return Message(version + b' 408 Request Timeout\r\nAge: 0\r\n\r\n')
+
+
+def timeout_message(request: Message):
+    _, _, version = request.line.split(b' ', 2)
+    return Message(version + b' 408 Request Timeout\r\nAge: 0\r\n\r\n')
